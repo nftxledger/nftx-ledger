bb802b4124d84007ec77b37d47e9fe0d5662d9cb
Abid Omar <contact@omarabid.com>
2023-08-25 16:58:08 +0700

    chore: add hardhat/ts configs

---
+++ hardhat.config.ts
@@ -0,0 +1,42 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ledger";
+import "@nomiclabs/hardhat-solhint";
+import '@openzeppelin/hardhat-upgrades';
+import dotenv from "dotenv";
+
+// Load .env file
+dotenv.config();
+
+// Check .env file variables 
+if (!process.env.ALCHEMY_API_KEY) {
+    throw new Error("Please set your ALCHEMY_API_KEY in a .env file");
+}
+if (!process.env.SEPOLIA_PRIVATE_KEY) {
+    throw new Error("Please set your SEPOLIA_PRIVATE_KEY in a .env file");
+}
+if (!process.env.ETHERSCAN_API_KEY) {
+    throw new Error("Please set your ETHERSCAN_API_KEY in a .env file");
+}
+
+// Hardhat Config
+const config: HardhatUserConfig = {
+    solidity: "0.8.21",
+    networks: {
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+            gasPrice: 5000000000,
+        },
+        hardhat: {
+            ledgerAccounts: [
+            ],
+        }
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+};
+
+
+export default config;
